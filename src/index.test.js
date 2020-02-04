8ab794515b6395344999ba3c22c1c1cf79210174
Olowogoke Seun <seunolowogoke@gmail.com>
2020-02-04 12:50:23 +0100

    updated jsdom test

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import jsdom from 'jsdom';
 import fs from 'fs';
 
+const { JSDOM } = jsdom;
 
 describe('Our first test', () => {
   it('should pass', () => {
@@ -10,13 +11,18 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-  it('should say hello', (done) => {
-    const index = fs.readFileSync('./src/index.html', 'utf-8');
-    jsdom.env(index, function(err, window) {
-      const h1 = window.document.querySelectorAll('h1')[0];
-      expect(h1.innerHTML).to.equal('Hello World!');
-      done();
-      window.close();
-    });
-  })
-})
+  before(() => {
+    return JSDOM.fromFile('./src/index.html')
+      .then((dom) => {
+        global.window = dom.window;
+        global.document = window.document;
+      });
+  });
+
+  it('should say hello', () => {
+    // const index = fs.readFileSync('./src/index.html', 'utf-8');
+    // jsdom.env(index, function(err, window) {
+    const h1 = document.querySelectorAll('h1')[0];
+    expect(h1.innerHTML).to.equal('Hello World!');
+  });
+});
